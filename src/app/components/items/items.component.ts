9186392f1d1d680d766c64bf07e6b60db848fe98
franakol <akolfrancis40@gmail.com>
2023-02-12 23:59:55 +0300

    can add items

--- src/app/components/items/items.component.ts
+++ src/app/components/items/items.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+
 import { ActivatedRoute, Router} from '@angular/router';
+import { bucketlistData, itemData} from '../bucketlist/bucket';
 
 @Component({
   selector: 'app-items',
@@ -8,26 +9,47 @@ import { ActivatedRoute, Router} from '@angular/router';
   styleUrls: ['./items.component.css']
 })
 export class ItemsComponent implements OnInit {
-
-    bucketlists: any;
+  bucketlist: bucketlistData = {
+    bucket_id: 0,
+    name: '',
+    items: [],
+    date_created: new Date(),
+    date_updated: new Date()
+  };
+  
     bucketName: any;
-    bucketlist: any;
     selectedBucketlistName!: string;
     itemName!: string;
+p: string|number|undefined;
 
   constructor(private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
     this.selectedBucketlistName = this.route.snapshot.paramMap.get('name') || 'Default Value';
-    this.itemName = this.route.snapshot.paramMap.get('itemName') || 'Add Item'
-
-
-  }
-  addItem(bucketlist: { name: any; }, itemName: any) {
-    this.router.navigate(['/items', {name: bucketlist.name, itemName: itemName}]);
+    const savedBucketlist = localStorage.getItem(this.selectedBucketlistName);
+    if (savedBucketlist) {
+      this.bucketlist = JSON.parse(savedBucketlist);
+    } else {
+      this.bucketlist.name = this.selectedBucketlistName;
+    }
+  
   }
   
- 
+  addItem(itemName: string) {
+    const newItem: itemData = {
+      item_id: Math.floor(Math.random() * 100), // generate a unique item_id
+      name: itemName,
+      date_created: new Date(),
+      date_updated: new Date()
+    };
+
+    this.bucketlist.items.push(newItem);
+
+    // save the updated bucketlist to local storage
+    localStorage.setItem(this.bucketlist.name, JSON.stringify(this.bucketlist));
+  }
+
+   
     
 }
 
